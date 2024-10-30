1fcc874bc887b67df0cdeb88f51efe34e50e3b63
Sloan Gauthier <sloangauthier@gmail.com>
2024-10-30 13:55:04 +0100

    Create interface for Animal, Association and Family + Type the state animal and the function getRandomizeAnimals

---
+++ src/Interfaces/IAnimal.ts
@@ -0,0 +1,23 @@
+import { IAssociation } from "./IAssociation.ts";
+import { IFamily } from "./IFamily.ts";
+
+interface IAnimal {
+    id: number;
+    name: string;
+    age: number;
+    species: string;
+    race: string | null;
+    gender: string;
+    size: string;
+    description: string;
+    availability: boolean;
+    url_image: string | null;
+    family_id: number | null;
+    family: IFamily | null;
+    association_id: number;
+    association: IAssociation | null;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export { IAnimal };
