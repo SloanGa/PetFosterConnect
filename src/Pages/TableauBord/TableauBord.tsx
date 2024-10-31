abdd570f32d0b00f110210a004dd7237c21403c9
camille <camille.nerriere@yahoo.fr>
2024-10-31 10:07:29 +0100

    dashboard page environnement created

---
+++ src/Pages/TableauBord/TableauBord.tsx
@@ -0,0 +1,18 @@
+import "./TableauBord.scss";
+import Header from "../../Components/Header/Header.tsx";
+import Footer from "../../Components/Footer/Footer.tsx";
+import AnimalCard from "../../Components/AnimalCard/AnimalCard.tsx";
+import AppLink from "../../Components/AppLink/AppLink.tsx";
+import Loading from "../../Components/Loading/Loading.tsx";
+// import { Error } from "../../Components/Error/Error.tsx";
+import { IAnimal } from "../../Interfaces/IAnimal.ts";
+
+const TableauBord = () => {
+	return (
+		<div>
+			<h1>Dashboard</h1>
+		</div>
+	);
+};
+
+export default TableauBord;
