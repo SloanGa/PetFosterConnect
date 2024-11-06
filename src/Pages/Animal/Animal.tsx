34f4d6ddef7a6e449b1ae43f04b0b5a186a6f1d5
Clara Mauro <maurog.clara@gmail.com>
2024-11-06 18:24:25 +0400

    page animal static done

--- src/Pages/Animal/Animal.tsx
+++ src/Pages/Animal/Animal.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Map from "../../Components/Map/Map";
+import AppLink from "../../Components/AppLink/AppLink";
+
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 import "./Animal.scss";
 
 const Animal = () => {
     // const { name-id } = useParams();
+    // Pour la modale de confirmation d'envoi d'une demande
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleClickConfirmBtn = async () => {
+        handleClose();
+    };
+
     return (
         <>
             <Header />
@@ -60,21 +75,84 @@ const Animal = () => {
                         </div>
                     </section>
                     <section className="section__association">
-                        <div id="map">
+                        <div className="map__container">
                             <Map />
                         </div>
-                        <div>SPA du 44</div>
-                        <div>Les Landes Bigot</div>
-                        <div>
-                            <span>44340</span>
-                            <span>Bouguenais</span>
+                        <div className="association__infos">
+                            <AppLink
+                                to={"/association/toto"}
+                                className={"btn association__name"}
+                                title={"Aller sur la page de l'association"}
+                                text={"SPA du 44"}
+                            />
+                            <div>Les Landes Bigot</div>
+                            <div>
+                                <span>44340</span>
+                                <span> </span>
+                                <span>Bouguenais</span>
+                            </div>
+                            <div>
+                                <span>Tel : </span>
+                                <a
+                                    href="tel:+111111111"
+                                    className="association__link"
+                                    title="Appeler l'association"
+                                    aria-label="Appeler l'association"
+                                >
+                                    02 49 62 81 02
+                                </a>
+                            </div>
+                            <div>
+                                <span>Mail : </span>
+                                <a
+                                    href="mailto:contact@SPA.com"
+                                    className="association__link"
+                                    title="Envoyer un mail à l'association"
+                                    aria-label="Envoyer un mail à l'association"
+                                >
+                                    contact@SPA.com
+                                </a>
+                            </div>
+                            {/* Si utilisateur connecté */}
+                            <button className="btn" onClick={handleShow}>
+                                Faire une demande d'accueil
+                            </button>
+                            {/* Si utilisateur non connecté */}
+                            {/* <AppLink
+                                to={"/inscription/famille"}
+                                className={"btn"}
+                                title={"S'inscrire pour faire une demande"}
+                                text={"Faire une demande d'accueil"}
+                            /> */}
                         </div>
-                        <div>02 49 62 81 02</div>
                     </section>
                 </div>
             </main>
 
             <Footer />
+
+            <Modal show={show} onHide={handleClose} centered className={"modal__confirm"}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmez-vous cette demande ?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Si vous confirmez votre demande, celle-ci sera envoyée à l'association qui vous
+                    contactera directement.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose} className={"btn--cancel"}>
+                        Annuler
+                    </Button>
+
+                    <Button
+                        variant="primary"
+                        onClick={handleClickConfirmBtn}
+                        className={"btn--confirm"}
+                    >
+                        Envoyer ma demande
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 };
