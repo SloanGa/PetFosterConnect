142a8643f4fc9386ecd1454bb8d06df18ec4fbdd
Sloan Gauthier <sloangauthier@gmail.com>
2024-11-13 16:52:38 +0100

    Update integration of profil page and add visual request on family profil

--- src/Pages/Profil/Profil.tsx
+++ src/Pages/Profil/Profil.tsx
@@ -13,6 +13,8 @@ import GestionEditEntityModal from "../../Components/GestionModal/GestionEditEnt
 import GestionModalDeleteEntity from "../../Components/GestionModal/GestionModalDeleteEntity.tsx";
 import Alert from "react-bootstrap/Alert";
 import { IUser } from "../../Interfaces/IUser.ts";
+import { Table } from "react-bootstrap";
+import Icon from "../../Components/Icon/Icon.tsx";
 
 interface ProfilProps {
     entity: IAssociation | IFamily | null;
@@ -21,7 +23,7 @@ interface ProfilProps {
     error: string | null;
     isLegitimate: boolean;
     setEntity: React.Dispatch<React.SetStateAction<IAssociation | IFamily | null>>;
-    userHasEntity : IUser | null
+    userHasEntity: IUser | null;
 }
 
 
@@ -34,7 +36,7 @@ const Profil = ({ entity, baseURL, isLoading, error, isLegitimate, setEntity, us
     const [showDelete, setShowDelete] = useState(false);
     const handleCloseDelete = () => setShowDelete(false);
     const handleShowDelete = () => setShowDelete(true);
-    
+
     return (
         <>
             <Helmet>
@@ -63,7 +65,8 @@ const Profil = ({ entity, baseURL, isLoading, error, isLegitimate, setEntity, us
                                 <div className="section__entity__header">
                                     <div className="entity__identity">
                                         <h1 className="main__title">{entity!.name}</h1>
-                                        <h2 className="main__subtitle">{entity!.description}</h2>
+                                        <img className="icon icon--title"
+                                             src="/src/assets/icons/id-card.svg" alt="" />
                                     </div>
                                 </div>
                                 <div className="section__entity__main">
@@ -76,21 +79,42 @@ const Profil = ({ entity, baseURL, isLoading, error, isLegitimate, setEntity, us
                                     <div className="entity__infos">
                                         <div className="entity__infos__data">
                                             <div className="entity__infos__data__item">
-                                                <p className="item__title">{entity!.name}</p>
 
-                                                <p className="item__title">{entity!.phone_number}</p>
-                                                <p className="item__title">
-                                                    {entity!.address} - {entity?.zip_code} {entity?.city}
-                                                </p>
-                                                {entity && "email_association" in entity ? (<a
-                                                    href={`mailto:${entity!.email_association}`}
-                                                    className="item__title"
-                                                >
-                                                    {entity!.email_association}
-                                                </a>) : <a
-                                                    href={`mailto:${userHasEntity?.email}`}
-                                                    className="item__title"
-                                                > {userHasEntity?.email} </a>}
+                                                <div className="infos__container">
+                                                    <img className="icon"
+                                                         src="/src/assets/icons/heart-handshake.svg" alt="" />
+                                                    <p className="item__title">{entity!.name}</p>
+                                                </div>
+
+                                                <div className="infos__container">
+                                                    <img className="icon"
+                                                         src="/src/assets/icons/phone.svg" alt="" />
+
+                                                    <p className="item__title">{entity!.phone_number}</p>
+                                                </div>
+
+                                                <div className="infos__container">
+                                                    <img className="icon"
+                                                         src="/src/assets/icons/house.svg" alt="" />
+                                                    <p className="item__title">
+                                                        {entity!.address} - {entity?.zip_code} {entity?.city}
+                                                    </p>
+                                                </div>
+
+                                                <div className="infos__container">
+                                                    <img className="icon"
+                                                         src="/src/assets/icons/mail.svg" alt="" />
+                                                    {entity && "email_association" in entity ? (<a
+                                                        href={`mailto:${entity!.email_association}`}
+                                                        className="item__title"
+                                                    >
+                                                        {entity!.email_association}
+                                                    </a>) : <a
+                                                        href={`mailto:${userHasEntity?.email}`}
+                                                        className="item__title"
+                                                    > {userHasEntity?.email} </a>}
+                                                </div>
+
                                             </div>
                                             {entity && "email_association" in entity ? <NavLink
                                                 to={`/animaux?association_id=${entity?.id}`}
@@ -104,6 +128,8 @@ const Profil = ({ entity, baseURL, isLoading, error, isLegitimate, setEntity, us
                                         </div>
                                     </div>
                                 </div>
+
+                                <p className="description">{entity!.description}</p>
                                 {isLegitimate ?
                                     <div className="btn__container">
                                         <button className=" btn btn--profil" onClick={handleShowEdit}>Modifier le profil
@@ -114,7 +140,7 @@ const Profil = ({ entity, baseURL, isLoading, error, isLegitimate, setEntity, us
                                         </button>
                                     </div> : null}
                             </section>
-                            <section className="section__map">
+                            <section className="section__bottom">
                                 {/* Vérifie si c'est une association ou une famille à afficher */}
                                 {entity && "email_association" in entity ? (<>
                                         <div className="map__container map__container--entity">
@@ -126,7 +152,55 @@ const Profil = ({ entity, baseURL, isLoading, error, isLegitimate, setEntity, us
                                         </div>
 
                                     </>
-                                ) : null}
+                                ) : (
+                                    <div className="request">
+                                        <h2 className="request__title">Demande en cours </h2>
+                                        <Table striped bordered responsive className={"text-center"}>
+                                            <thead>
+                                            <tr>
+                                                <th>N°</th>
+                                                <th>Nom animal</th>
+                                                <th>Email association</th>
+                                                <th>Date de la demande</th>
+                                                <th>Statut de la demande</th>
+                                                {isLegitimate ? <th>Supprimer</th> : null}
+
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            {[1, 2, 3].map((request, index) => (
+                                                <tr key={index}>
+                                                    <td>{index + 1}</td>
+                                                    <td>
+                                                        <a href="/animaux/oscar-1" className="link">
+                                                            Oscar
+                                                        </a>
+                                                    </td>
+                                                    <td>
+                                                        <a href="mailto:contact@animalrescue.fr" className="link">
+                                                            contact@animalrescue.fr
+                                                        </a>
+                                                    </td>
+                                                    <td>01/11/2024</td>
+                                                    <td>
+                                                        <span>En attente</span>
+                                                    </td>
+                                                    <td className="delete">
+                                                        {isLegitimate ? (
+                                                            <Icon
+                                                                ariaLabel={"Supprimer la demande"}
+                                                                src={"/src/assets/icons/trash.svg"}
+                                                                alt={"icône supprimer"}
+                                                                onClick={() => console.log("test")}
+                                                            />
+                                                        ) : null}
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                            </tbody>
+                                        </Table>
+                                    </div>
+                                )}
 
                             </section>
 
