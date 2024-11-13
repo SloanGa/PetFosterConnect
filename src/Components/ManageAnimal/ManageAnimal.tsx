87478b8faa49314970e1f716d8a50e49bd741836
camille <camille.nerriere@yahoo.fr>
2024-11-13 12:12:41 +0100

    add path to dashboard card

--- src/Components/ManageAnimal/ManageAnimal.tsx
+++ src/Components/ManageAnimal/ManageAnimal.tsx
@@ -269,7 +269,7 @@ const ManageAnimal = () => {
 								<DashboardCard
 									onShowDeleteModal={handleShowDeleteModal}
 									onShowGestionModal={handleShowGestionModal}
-									path={""}
+									path={`/animaux/${animal.slug}`}
 									src={`${baseURL}${animal.url_image}`}
 									alt={animal.name}
 									name={animal.name}
