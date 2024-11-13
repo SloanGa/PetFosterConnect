425662c9db179f7273ebdeb374347d6aafc5cfe1
Clara Mauro <maurog.clara@gmail.com>
2024-11-13 20:40:05 +0400

    map on data

--- src/Components/ManageRequest/ManageRequest.tsx
+++ src/Components/ManageRequest/ManageRequest.tsx
@@ -2,6 +2,7 @@ import { Table, Form } from "react-bootstrap";
 import "./ManageRequest.scss";
 import Icon from "../Icon/Icon";
 import { useEffect, useState } from "react";
+import { IAnimal } from "../../Interfaces/IAnimal";
 
 const baseURL = import.meta.env.VITE_API_URL;
 
@@ -13,23 +14,51 @@ const ManageRequest = () => {
     const [requests, setRequests] = useState([]);
     // Faire interface
 
-    useEffect(async () => {
-        try {
-            const token = localStorage.getItem("auth_token");
-            const response = await fetch(`${baseURL}/dashboard/association/request`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            const result = await response.json();
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
+    useEffect(() => {
+        const fetchRequests = async () => {
+            try {
+                const token = localStorage.getItem("auth_token");
+                const response = await fetch(`${baseURL}/dashboard/association/request`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                if (response.ok) {
+                    let requests = await response.json();
+                    // Formater la date en jour/mois/année
+                    requests = requests.map((request) => ({
+                        ...request,
+                        formattedDate: new Date(request.created_at).toLocaleDateString("fr-FR"),
+                    }));
+                    // On groupe les demandes par animaux
+                    const groupedRequests = requests.reduce((acc, request) => {
+                        const animalGroup = acc.find(
+                            (group) => group[0].animal_id === request.animal_id
+                        );
+                        if (animalGroup) {
+                            animalGroup.push(request);
+                        } else {
+                            acc.push([request]);
+                        }
+                        return acc;
+                    }, []);
+                    setRequests(groupedRequests);
+                } else {
+                    // Afficher une erreur
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchRequests();
     }, []);
 
-    const handleClickOnEditStatus = (index: number) => {
+    const handleClickOnEditStatus = (index: number, status: string) => {
         setEditingIndex(index === editingIndex ? null : index);
+        if (index !== editingIndex) {
+            setSelectedStatus(status);
+        }
     };
 
     const handleClickOnSaveNewStatus = () => {
@@ -38,81 +67,93 @@ const ManageRequest = () => {
 
     return (
         <div className="manage-request">
-            <section className="request">
-                <div className="request__header">
-                    <img src={`${baseURL}/images/animals/Oscar-1.webp`} alt="" loading="lazy" />
-                    <h2>
-                        <a href="/animaux/oscar-1" className="link">
-                            Oscar
-                        </a>
-                    </h2>
-                </div>
-                <Table striped bordered responsive className={"text-center"}>
-                    <thead>
-                        <tr>
-                            <th>N°</th>
-                            <th>Famille</th>
-                            <th>Email</th>
-                            <th>Date de la demande</th>
-                            <th>Statut de la demande</th>
-                            <th>Modifier</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {[1, 2, 3].map((request, index) => (
-                            <tr key={index}>
-                                <td>{index + 1}</td>
-                                <td>
-                                    <a href="/famille/dupont-1" className="link">
-                                        Dupont
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href="mailto:dupont@gmail.com" className="link">
-                                        dupont@gmail.com
-                                    </a>
-                                </td>
-                                <td>01/11/2024</td>
-                                <td>
-                                    {editingIndex === index ? (
-                                        <Form.Select
-                                            name="status"
-                                            value={selectedStatus}
-                                            onChange={(e) => setSelectedStatus(e.target.value)}
-                                        >
-                                            <option value="En attente">En attente</option>
-                                            <option value="Acceptée">Acceptée</option>
-                                            <option value="Refusée">Refusée</option>
-                                        </Form.Select>
-                                    ) : (
-                                        <span>En attente</span>
-                                    )}
-                                </td>
-                                {editingIndex === index ? (
-                                    <td>
-                                        <button
-                                            type="button"
-                                            className="btn btn--save-status"
-                                            onClick={handleClickOnSaveNewStatus}
-                                        >
-                                            Ok
-                                        </button>
-                                    </td>
-                                ) : (
-                                    <td className="edit">
-                                        <Icon
-                                            ariaLabel={"Modifier le statut de la demande"}
-                                            src={"/src/assets/icons/pen.svg"}
-                                            alt={"icône modifier"}
-                                            onClick={() => handleClickOnEditStatus(index)}
-                                        />
-                                    </td>
-                                )}
+            {requests.map((animalGroup) => (
+                <section className="request" key={animalGroup[0].animal.id}>
+                    <div className="request__header">
+                        <img
+                            src={`${baseURL}${animalGroup[0].animal.url_image}`}
+                            alt=""
+                            loading="lazy"
+                        />
+                        <h2>
+                            <a href="/animaux/oscar-1" className="link">
+                                {animalGroup[0].animal.name}
+                            </a>
+                        </h2>
+                    </div>
+                    <Table striped bordered responsive className={"text-center"}>
+                        <thead>
+                            <tr>
+                                <th>N°</th>
+                                <th>Famille</th>
+                                <th>Email</th>
+                                <th>Date de la demande</th>
+                                <th>Statut de la demande</th>
+                                <th>Modifier</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </section>
+                        </thead>
+                        <tbody>
+                            {animalGroup.map((request, index) => (
+                                <tr key={request.id}>
+                                    <td>{request.id}</td>
+                                    <td>
+                                        <a
+                                            href={`/famille/${request.family.slug}`}
+                                            className="link"
+                                        >
+                                            {request.family.name}
+                                        </a>
+                                    </td>
+                                    <td>
+                                        {/* TODO Ajouter user dans le back pour recup mail */}
+                                        <a href={`mailto:{dupont@gmail.com}`} className="link">
+                                            dupont@gmail.com
+                                        </a>
+                                    </td>
+                                    <td>{request.formattedDate}</td>
+                                    <td>
+                                        {editingIndex === index ? (
+                                            <Form.Select
+                                                name="status"
+                                                value={selectedStatus}
+                                                onChange={(e) => setSelectedStatus(e.target.value)}
+                                            >
+                                                <option value="En attente">En attente</option>
+                                                <option value="Acceptée">Acceptée</option>
+                                                <option value="Refusée">Refusée</option>
+                                            </Form.Select>
+                                        ) : (
+                                            <span>{request.status}</span>
+                                        )}
+                                    </td>
+                                    {editingIndex === index ? (
+                                        <td>
+                                            <button
+                                                type="button"
+                                                className="btn btn--save-status"
+                                                onClick={handleClickOnSaveNewStatus}
+                                            >
+                                                Ok
+                                            </button>
+                                        </td>
+                                    ) : (
+                                        <td className="edit">
+                                            <Icon
+                                                ariaLabel={"Modifier le statut de la demande"}
+                                                src={"/src/assets/icons/pen.svg"}
+                                                alt={"icône modifier"}
+                                                onClick={() =>
+                                                    handleClickOnEditStatus(index, request.status)
+                                                }
+                                            />
+                                        </td>
+                                    )}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </section>
+            ))}
         </div>
     );
 };
