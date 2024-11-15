36ea79140b88f0dcad491ece7215e25ae389e2e8
camille <camille.nerriere@yahoo.fr>
2024-11-15 16:45:05 +0100

    connect profil to dashboard

--- src/Pages/TableauBord/TableauBord.tsx
+++ src/Pages/TableauBord/TableauBord.tsx
@@ -8,51 +8,57 @@ import { useAuth } from "../../Context/AuthContext.tsx";
 import { Navigate, Routes, Route } from "react-router-dom";
 import ManageAnimal from "../../Components/ManageAnimal/ManageAnimal.tsx";
 import ManageRequest from "../../Components/ManageRequest/ManageRequest.tsx";
+import Profil from "../Association/Association.tsx";
 
 const TableauBord = () => {
-    const { userData } = useAuth();
-    const [isAssociationConnected, setIsAssociationConnected] = useState<boolean | null>(null);
+	const { userData } = useAuth();
+	const [isAssociationConnected, setIsAssociationConnected] = useState<
+		boolean | null
+	>(null);
 
-    useEffect(() => {
-        const storedUser = JSON.parse(localStorage.getItem("user"));
+	const storedUser = JSON.parse(localStorage.getItem("user"));
 
-        if (!storedUser || !(storedUser.role === "association")) {
-            setIsAssociationConnected(false);
-        } else {
-            setIsAssociationConnected(true);
-        }
-    }, []);
+	useEffect(() => {
+		const storedUser = JSON.parse(localStorage.getItem("user"));
 
-    if (isAssociationConnected === false) {
-        return <Navigate to="/connexion" />;
-    }
+		if (!storedUser || !(storedUser.role === "association")) {
+			setIsAssociationConnected(false);
+		} else {
+			setIsAssociationConnected(true);
+		}
+	}, []);
 
-    return (
-        <>
-            <Helmet>
-                <title>Tableau de bord | PetFoster Connect</title>
-                <meta
-                    name="description"
-                    content="PetFosterConnect permet de mettre en relation des familles d’accueil pour les animaux et des associations de protection animale en accueillant des animaux en attendant leur adoption définitive afin de leur offrir une meilleure vie."
-                />
-            </Helmet>
+	if (isAssociationConnected === false) {
+		return <Navigate to="/connexion" />;
+	}
 
-            <Header />
-            <div className="content">
-                <LeftNavBar />
-                <main className="main__content">
-                    <h1 className="main__title">
-                        Tableau de bord | {userData ? userData.association.name : "XXX"}
-                    </h1>
-                    <Routes>
-                        <Route path="/" element={<ManageAnimal />} />
-                        <Route path="/demandes" element={<ManageRequest />} />
-                    </Routes>
-                </main>
-            </div>
-            <Footer />
-        </>
-    );
+	return (
+		<>
+			<Helmet>
+				<title>Tableau de bord | PetFoster Connect</title>
+				<meta
+					name="description"
+					content="PetFosterConnect permet de mettre en relation des familles d’accueil pour les animaux et des associations de protection animale en accueillant des animaux en attendant leur adoption définitive afin de leur offrir une meilleure vie."
+				/>
+			</Helmet>
+
+			<Header />
+			<div className="content">
+				<LeftNavBar />
+				<main className="main__content">
+					<h1 className="main__title">
+						Tableau de bord | {userData ? userData.association.name : "XXX"}
+					</h1>
+					<Routes>
+						<Route path="/" element={<ManageAnimal />} />
+						<Route path="/demandes" element={<ManageRequest />} />
+						<Route path="/profil-association/:slug" element={<Profil />} />
+					</Routes>
+				</main>
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default TableauBord;
