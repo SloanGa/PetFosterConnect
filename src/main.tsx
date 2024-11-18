6f09effe62b48ea084da5f846f56e6d9779f41a0
Clara Mauro <maurog.clara@gmail.com>
2024-11-18 15:28:59 +0400

    fix TS errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "/src/styles/reset.css";
 import "/src/styles/style.scss";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-    //<StrictMode>
-    <App />
-    //</StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
