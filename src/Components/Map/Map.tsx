596aa751e618664f65e88197cd0060cdeb047eb4
camille <camille.nerriere@yahoo.fr>
2024-11-19 16:00:07 +0100

    fix leaflet and url errors

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,24 +1,36 @@
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { LatLngTuple } from "leaflet";
+import { LatLngTuple, L } from "leaflet";
 
 interface MapProps {
-    longitude: number;
-    latitude: number;
+	longitude: number;
+	latitude: number;
 }
 
-const Map = ({ longitude, latitude }: MapProps) => {
-    const position: LatLngTuple = [latitude, longitude];
+// Définir le chemin des icônes Leaflet
+L.Icon.Default.mergeOptions({
+	iconUrl: "/assets/leaflet/marker-icon.png", // Chemin vers l'icône principale
+	iconRetinaUrl: "/assets/leaflet/marker-icon-2x.png", // Chemin vers l'icône Retina
+	shadowUrl: "/assets/leaflet/marker-shadow.png", // Chemin vers l'ombre
+});
 
-    return (
-        <MapContainer center={[latitude, longitude]} zoom={14} scrollWheelZoom={true} className="map">
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}></Marker>
-        </MapContainer>
-    );
+const Map = ({ longitude, latitude }: MapProps) => {
+	const position: LatLngTuple = [latitude, longitude];
+
+	return (
+		<MapContainer
+			center={[latitude, longitude]}
+			zoom={14}
+			scrollWheelZoom={true}
+			className="map"
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={position}></Marker>
+		</MapContainer>
+	);
 };
 
 export default Map;
